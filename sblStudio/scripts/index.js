28747a9ea6d093c67f17891c913a5cb04b9e562e
Daniil <70716111+DragonLorde@users.noreply.github.com>
2020-09-09 12:12:40 +0500

    Add files via upload

--- sblStudio/scripts/index.js
+++ sblStudio/scripts/index.js
@@ -17,19 +17,44 @@ let aboutCard = document.querySelectorAll(".about__column");
         prop.addEventListener("mouseenter" , (e) => {
             if(e.target.classList.contains("about__column") ) {
                 let hv = e.target.querySelector(".about__hv");
-                hv.classList.remove("about__column__hide");
+                setTimeout(() => { 
+                    hv.classList.remove("about__column__hide");
+                    e.target.querySelector(".hide__button").classList.add("bt__op");
+                    e.target.querySelector(".hide__button").style.display = "block"; 
+                } , 600);
+                
+                setTimeout(() => { 
+                    hv.classList.add("hide__hides");
+                    e.target.querySelector(".hide__button").classList.remove("bt__op"); 
+                    e.target.querySelector(".hide__button").classList.add("hide__hides");
+            } , 700);
                 let dec = e.target.querySelector("p");
                 dec.style.textalign = "left";
                 dec.style.color = "#FFEE90";
                 e.target.querySelector(".hide_hide").style.display = "none";
-                e.target.querySelector(".hide__button").style.display = "block"; 
+                
+                
+
             }   
         });
         prop.addEventListener("mouseleave" , (e) => {
             if(e.target.classList.contains("about__column") ) {
                 let hv = e.target.querySelector(".about__hv");
-                hv.classList.add("about__column__hide");
+                
                 e.target.querySelector("p").style.color = "#FFFFFF";
+               // hv.classList.remove("hide__hides");
+                //setTimeout(() => hv.classList.add("about__column__hide") , 0);
+                setTimeout(() => { 
+                    hv.classList.add("about__column__hide");
+                    e.target.querySelector(".hide__button").classList.remove("bt__op");
+                    e.target.querySelector(".hide__button").style.display = "none"; 
+                } , 610);
+                
+                setTimeout(() => { 
+                    hv.classList.add("hide__hides");
+                    e.target.querySelector(".hide__button").classList.remove("bt__op"); 
+                    e.target.querySelector(".hide__button").classList.add("hide__hides");
+            } , 710);
                 e.target.querySelector(".hide_hide").style.display = "block";
                 e.target.querySelector(".hide__button").style.display = "none"; 
             }  
